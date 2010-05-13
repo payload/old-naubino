f19a3ec933fce12715e9070e5eaa46b674069fd7
Gilbert R. Röhrbein <s1394474@mail.zih.tu-dresden.de>
2010-05-13 16:03:44 +0200

    cleaned merge

--- domino/src/Game.java
+++ domino/src/Game.java
@@ -54,98 +54,7 @@ public class Game {
 	}
 
 	public void refresh() {
-		physik();
-	}
-
-	@SuppressWarnings("unused")
-	private void gravity(Ball b) {
-		b.acceleration.add(new Vektor(b.position, getCenter(), 
-			4 / b.distanceTo(getCenter()).getLength()));
-	}
-	
-	private void indirectGravity(Ball b) {
-		//Vektor v = new Vektor(b.position, getCenter(),
-		//		b.distanceTo(getCenter()).getLength() * 0.0001 + 0.2);
-		Vektor difference = b.position.sub(getCenter());
-		double length = difference.getLength();
-		difference.setLength(length * 0.0001 + 0.2);
-		b.accelerate(difference);
-	}
-	
-	/* Federkraefte zwischen gejointen Baellen */
-	private void swingBalls(Joint j) {
-		Vektor real_diff   = j.a.position.sub(j.b.position);
-		double real_length = real_diff.getLength();
-		double wish_length = j.getLength(); 
-		Vektor wish_diff   = Vektor.polar(real_diff.getAngle(), wish_length);
-		Vektor force       = wish_diff.sub(real_diff);
-		force = force.mul((real_length / wish_length) * j.getStrength());
-		j.a.accelerate(force);
-		j.b.accelerate(force.mul(-1));
-	}
-	
-	/* Kollisions behandlung */
-	private void collide(Collision c) {
-		/* farben vergleichen
-		 * TODO besseres Joinen von Balls*/
-//		if (c.a.color.equals(c.b.color))
-//			join(c.a, c.b);
-		Vektor diff2 = c.diff.mul(0.05);
-		c.a.accelerate(diff2.mul(-1));
-		c.b.accelerate(diff2);
-	}
-	
-	private void moveBall(Ball b) {
-		b.speed = b.speed.add(b.acceleration);
-		b.position = b.position.add(b.speed);
-	}
-	
-	private void friction(Ball b) {
-		b.accelerate(b.speed.mul(-0.2));
-	}
-	
-	private void moveActiveBall() {
-		active.accelerate(active.position.sub(pointer));
-	}
-	
-	public void physik() {
-		List<Collision> collisions = new LinkedList<Collision>();
-		collisions = Collections.synchronizedList(collisions);
-		int balls_count = balls.size();
-		if (balls_count > 1) {
-			for (int i = 0; i < balls_count-1; i++) {
-				for (int j = i+1; j < balls_count; j++) {
-				
-					Collision collision = Collision.test(balls.get(i), balls.get(j));
-					if (collision != null)
-						collisions.add(collision);
-				}
-			}
-		}
-		if (active != null) moveActiveBall();
-		
-		for (Ball b : balls) {
-			b.acceleration = new Vektor();
-			// gravity(b);
-			indirectGravity(b);
-			//repulseOtherBalls(b);
-			friction(b);
-		}
-		for (Collision c : collisions)
-			collide(c);
-		for (Joint j : joints)
-			swingBalls(j);
-		for (Ball b : balls)
-			moveBall(b);
-	}
-
-	public void setFieldSize(float fieldSize) {
-		this.fieldSize = fieldSize;
-	}
-
-
-	public Ball getBall(int index) {
-		return balls.get(index);
+		physics.physik();
 	}
 
 	private Ball createBall(Vektor v) {
@@ -174,7 +83,7 @@ public class Game {
 
 	/* ersetzt einen gleichfarbigen ball im spiel und kuemmert sich um alle Joints*/
 	private void replaceBall(Ball a, Ball b) {
-}
+	}
 	
 	private Ball collidingBall(Vektor v) {
 		for (Ball b : balls)
