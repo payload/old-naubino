e1b7f3b9c764456f977e1652064ac437db614420
Gilbert R. Röhrbein <s1394474@mail.zih.tu-dresden.de>
2010-05-15 15:57:40 +0200

    random angle of create pair as a full circle

--- domino/src/Game.java
+++ domino/src/Game.java
@@ -166,7 +166,7 @@ public class Game {
 
 	private void createPair(Vektor v) {
 		Vektor pair = new Vektor(1, 0).mul(Joint.defaultLength * 1.5);
-		pair.setAngle(rand.nextDouble() * Math.PI);
+		pair.setAngle(rand.nextDouble() * Math.PI * 2);
 		Vektor pos1 = v.add(pair);
 		Vektor pos2 = v.sub(pair);
 		Ball ball1 = createBall(pos1);
