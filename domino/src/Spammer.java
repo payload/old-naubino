ae27e7279e4ac627fcaa25d60169e18d56e5cbf3
Hendrik Sollich <Hendrik@Dean>
2010-05-19 23:58:48 +0200

    rewrote Spammer.randomPair

--- domino/src/Spammer.java
+++ domino/src/Spammer.java
@@ -1,39 +1,27 @@
 import java.util.Random;
 
-
 class Spammer {
-	
+
 	private Game game;
 	private Random rand = new Random();
-	
+
 	public Spammer(Game game) {
 		this.game = game;
 	}
-	
+
+	private double randomAngle() {
+		return rand.nextDouble() * Math.PI * 2;
+	}
+
 	public void randomPair() {
-		Vektor randPos;
-		// TODO randomPair() generiert keine "unten rechts" Baelle
-		double xMargin = game.height / 2 - game.getFieldSize() / 2;
-		double yMargin = game.width / 2 - game.getFieldSize() / 2;
-		switch (rand.nextInt(3)) {
-		case 0:
-			randPos = new Vektor(rand.nextDouble() * xMargin, rand.nextDouble() * yMargin);
-			break;
-		case 1:
-			randPos = new Vektor(game.width - rand.nextDouble() * xMargin, rand.nextDouble() * yMargin);
-			break;
-		case 2:
-			randPos = new Vektor(rand.nextDouble() * xMargin, game.height - rand.nextDouble() * yMargin);
-			break;
-		case 3:
-			randPos = new Vektor(game.width - rand.nextDouble() * xMargin, game.height - rand.nextDouble() * yMargin);
-			System.out.println("unten rechts");
-			break;
-		default:
-			randPos = new Vektor(game.width - rand.nextDouble() * xMargin, game.height - rand.nextDouble() * yMargin);
-			System.out.println("unten rechts");
-			break;
-		}
-		game.createPair(randPos);
+		double field_size = game.getFieldSize();
+		Vektor v = Vektor.polar(randomAngle(), (field_size));
+		v = game.getCenter().add(v);
+		game.createPair(v);
+	}
+
+	public void randomPairs() {
+		for (int i = 0; i < 100; i++)
+			randomPair();
 	}
 }
