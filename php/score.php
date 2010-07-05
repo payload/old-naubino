8e77ecf0150b7bf870e3bfb63c52beb38b08ba48
Hendrik Sollich <Hendrik.Sollich@gmx.de>
2010-07-05 12:26:58 +0200

    added rss feature to score (score.php?type=rss)

--- php/score.php
+++ php/score.php
@@ -20,6 +20,32 @@ $s->formated_heros();
 
 <?php
 }
+elseif($_GET['type']=="rss"){
+
+echo '<?xml version="1.0" encoding="UTF-8"?>';
+ ?><rss version="2.0">
+
+<channel>
+<title>Naubino</title>
+<link><?php echo $SELF_PHP ?></link>
+<description>Highscore</description>
+<language>de</language>
+
+<?php
+	for($i=0;$i<count($s->heros);$i++){
+	?>
+	<item>
+		<title><?PHP echo $s->heros[$i][0] ." ".$s->heros[$i][1]?></title>
+	</item>
+	<?php
+	}
+?>
+</channel>
+</rss>
+
+
+<?PHP
+}
 else{
 header( 'Content-Type: text/plain' );
 $s->string_heros_backward();
